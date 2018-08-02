30494124fba3f4aade8428632cca9b5ecb4a60a2
cgood404 <chrisgood404@gmail.com>
2018-08-02 10:01:28 -0400

    handler for getting all content for a specified user

---
+++ AssertOwnership/RequestUserContent.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace AssertOwnership
+{
+    public class RequestUserContentHandler : IHttpHandler
+    {
+
+        /* Set the base url for the portal and get path of certificate */
+        private readonly string portalUrl = "https://fcg-arcgis-srv.freedom.local/portal/";
+        private OwnershipHelper helper = new OwnershipHelper();
+
+        public bool IsReusable { get { return true; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            /* ProcessRequest is automatically called by IIS when it receives a
+               request to the url pointed to by web.config */
+
+            // Get the username from the identity of the request (which was set by PKIAuthenticationModule)
+            string user = context.User.Identity.Name;
+            // Get the info for the user
+            JObject userInfo = helper.GetUserInfo(user);
+            // If the user doesn't exist, exit with 403 response (Forbidden)
+            if (userInfo["error"] != null || ((string)userInfo["level"]) != "2")
+            {
+                context.Response.StatusCode = 403;
+                return;
+            }
+
+            // Get the username and starting folder from the request parameters
+            HttpRequest request = context.Request;
+            string username = request.QueryString["username"];
+            string folder = request.QueryString["folder"];
+            // If a username is not specified, default to the user who made the request
+            if (username == null)
+            {
+                username = user;
+                return;
+            }
+            // If a folder is not specified, default to the root folder
+            if (folder == null)
+            {
+                folder = "/";
+            }
+
+            JObject items = GetItems(username, folder, true);
+            context.Response.Write(helper.JsonToString(items));
+        }
+
+        public JObject GetUserContent(string username, string folder)
+        {
+            return helper.StringToJson(helper.GetRequest("sharing/rest/content/users/" + username + "/" + folder,
+                                 new string[] { "f" },
+                                 new string[] { "json" }));
+        }
+
+        public JObject GetItems(string username, string folder, bool recursive)
+        {
+            JObject items = new JObject();
+            JObject userContent = GetUserContent(username, folder);
+
+            foreach (JToken item in userContent["items"])
+            {
+                items[item["id"]] = helper.GetItemInfo((string)item["id"]);
+            }
+
+            if (recursive)
+            {
+                foreach (JToken innerFolder in userContent["folders"])
+                    items.AddAfterSelf(GetItems(username, (string)innerFolder, true));
+            }
+
+            return items;
+        }
+    }
+}
