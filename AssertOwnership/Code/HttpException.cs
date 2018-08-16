611cffe208fb180d559de08e47be36ff0bfc5ccd
cgood <cgood@freedomconsultinggroup.com>
2018-08-16 15:07:59 -0400

    Make HttpException

---
+++ AssertOwnership/Code/HttpException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssertOwnership
+{
+    public class HttpException : Exception
+    {
+        private int statusCode;
+
+        public HttpException(string message, int statusCode) : base(message)
+        {
+            this.statusCode = statusCode;
+        }
+
+        public int StatusCode { get { return this.statusCode; } }
+    }
+}
